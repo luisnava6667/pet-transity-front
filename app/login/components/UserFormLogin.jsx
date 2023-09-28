9f67a5b2ea75fc86cf9ffb345761c2559b4196af
Gonzalo Rodriguez <gonzalo-ezequiel@hotmail.com>
2023-09-28 11:39:36 -0300

    instalacion de Yup, Formik, Axios y Sweetalert2. Agregue endpoint para login y validaciones

--- app/login/components/UserFormLogin.jsx
+++ app/login/components/UserFormLogin.jsx
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { useState } from "react";
 
 const UserFormLogin = () => {
@@ -6,6 +9,43 @@ const UserFormLogin = () => {
   const handleButtonClick = (button) => {
     setSelectedButton(button);
   };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("El correo electrónico es inválido")
+        .required("El correo electrónico es requerido"),
+      password: Yup.string()
+        .required("La contraseña es requerida")
+        .min(3, "La contraseña debe tener al menos 3 caracteres"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+
+      axios
+        .post(`https://api-pet-beak.onrender.com/usuarios/login`, values)
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  });
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleSubmit();
+    }
+  };
+
+  const { handleSubmit, handleChange, handleBlur, touched, errors } = formik;
+
   return (
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div class="flex flex-col items-center">
@@ -40,7 +80,11 @@ const UserFormLogin = () => {
       </div>
 
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form class="space-y-6">
+        <form
+          onKeyDown={handleKeyDown}
+          onSubmit={handleSubmit}
+          class="space-y-6"
+        >
           <div>
             <label
               for="email"
@@ -51,13 +95,24 @@ const UserFormLogin = () => {
             <div class="mt-2">
               <input
                 id="correo"
-                name="correo"
+                name="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
                 type="email"
                 autocomplete="correo"
                 required
                 placeholder="Correo electrónico"
-                class="block w-[25rem] h-12 p-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                class={`block w-[25rem] h-12 p-2 rounded-md py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ${
+                  touched.email && errors.email
+                    ? "ring-red-500  focus:ring-red-500"
+                    : "ring-gray-300 placeholder-text-gray-400 focus:ring-indigo-600"
+                } focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
               />
+              {touched.email && errors.email && (
+                <div className="flex flex-row-reverse w-[11.5rem] sm:w-[13.5rem] mt-5 text-red-500 text-xs sm:text-sm">
+                  {errors.email}
+                </div>
+              )}
             </div>
           </div>
 
@@ -74,12 +129,23 @@ const UserFormLogin = () => {
               <input
                 id="password"
                 name="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
                 type="password"
                 autocomplete="current-password"
                 placeholder="**********"
                 required
-                class="block w-[25rem] h-12 p-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                class={`block w-[25rem] h-12 p-2 rounded-md py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ${
+                  touched.password && errors.password
+                    ? "ring-red-500  focus:ring-red-500"
+                    : "ring-gray-300 placeholder-text-gray-400 focus:ring-indigo-600"
+                } focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
               />
+              {touched.password && errors.password && (
+                <div className="flex flex-row-reverse w-[11.5rem] sm:w-[13.5rem] mt-5 text-red-500 text-xs sm:text-sm">
+                  {errors.email}
+                </div>
+              )}
               <div class="text-sm mt-2 flex justify-end">
                 <a
                   href="#"
@@ -94,13 +160,13 @@ const UserFormLogin = () => {
           <div className="w-[25rem] flex flex-col items-center ">
             <button
               type="submit"
-              class="flex w-96 h-14 lg:text-2xl items-center justify-center rounded-md bg-[#E59D1C] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              class="flex w-96 h-14 lg:text-2xl items-center justify-center rounded-md bg-[#E59D1C] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Ingresar
             </button>
             <button
               type="submit"
-              class="flex mt-4 w-96 h-14 text-center items-center  border-2 border-[#4F3300] justify-center rounded-md bg-[#ccc4bb] px-3 py-1.5 text-sm lg:text-2xl font-semibold leading-6 text-[#303030] shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              class="flex mt-4 w-96 h-14 text-center items-center  border-2 border-[#4F3300] justify-center rounded-md bg-[#ccc4bb] px-3 py-1.5 text-sm lg:text-2xl font-semibold leading-6 text-[#303030] shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Registrarse
             </button>
