38cfbeefb92d59044b965b6527df6b4b11c6c279
luisnava6667 <navaluisrodolfo@gmail.com>
2023-10-25 11:37:54 -0300

    updates

--- app/page.js
+++ app/page.js
@@ -1,17 +1,31 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../assets/logo.svg'
-
+import donar from '../assets/donar.svg'
 export default function Home() {
   return (
     <main className='bg-[#CCC4BB] h-screen'>
-      <div className='flex flex-1 items-center justify-end md:justify-between'>
-        <Link className='block text-teal-600' href='/'>
+      <div className='grid px-20 justify-items-center md:flex md:justify-between'>
+        <Link className='block text-teal-600 md:w-1/2' href='/'>
           <Image className='' width={427} height={90} alt='logo' src={logo} />
         </Link>
-        <div className='flex items-center gap-4'>
-          <Link href='/login'>Login</Link>
-          <Link href='/donar'>Donar</Link>
+        <div className='items-center gap-4 flex my-5 md:my-0 text-center md:w-1/2'>
+          <Link
+            href='/login'
+            className='flex justify-evenly text-center items-center rounded-md bg-[#E59D1C] px-5 py-2.5 text-xl font-bold text-white transition md:w-1/3 h-10 shadow-md'>
+            Login
+          </Link>
+          <Link
+            href='/register'
+            className='flex justify-evenly text-center items-center rounded-md bg-[#E59D1C] px-5 py-2.5 text-xl font-bold text-white transition md:w-1/3 h-10 shadow-md'>
+            Registrarse
+          </Link>
+          <Link
+            href='/donar'
+            className='flex justify-evenly text-center items-center rounded-md bg-[#E59D1C] px-7 py-2.5 text-xl font-bold text-white transition md:w-1/3 h-10 shadow-md'>
+            <Image className='' width={20} height={20} alt='logo' src={donar} />
+            Donar
+          </Link>
         </div>
       </div>
       <div>
