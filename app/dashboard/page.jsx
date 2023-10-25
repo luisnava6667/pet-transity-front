9170ed27683e1de4c58469e429ee2599e30c96ce
luisnava6667 <navaluisrodolfo@gmail.com>
2023-10-25 06:41:08 -0300

    updates

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -6,9 +6,31 @@ import Sidebar from '../components/Sidebar'
 import TopBar from '../components/TopBar'
 import { SessionProvider, useSession } from 'next-auth/react'
 import SessionAuthProvider from '../context/SessionAuthProvider'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Page = () => {
-  const { status } = useSession()
+  const [user, setUser] = useState([])
+
+  const { data, status } = useSession()
+
+  useEffect(() => {
+    const userInfo = async () => {
+      try {
+        const url = `${process.env.NEXT_PUBLIC_URL}/usuarios/perfil`
+        const response = await axios.get(url, {
+          headers: {
+            'Authorization': `Bearer ${data.token}`,
+            'Content-Type': 'application/json'
+          }
+        })
+        return setUser(response.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    userInfo()
+  }, [data])
 
   if (status === 'loading') {
     return <div>Cargando...</div>
@@ -20,8 +42,7 @@ const Page = () => {
         <TopBar />
         <div className='w-full flex flex-col items-center justify-center'>
           <div className='flex w-[65.4rem] gap-4 '>
-            
-            <CardUsuarioDashboard />
+            <CardUsuarioDashboard user={user} />
             <InformacionCasaDashboard />
           </div>
           <CardGrandeDashboard />
